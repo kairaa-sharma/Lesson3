ef9abd747ec6d6be22c62ae6de04fb84b5515761
Kairaa2014 <kairaasharma22@gmail.com>
2025-10-17 12:06:34 +0530

    demo

---
+++ Finding Types/types.py
@@ -0,0 +1,12 @@
+# Let's check the data types of different types of values 
+a = 5
+print("type of a: ", type(a))
+
+b = 2.5
+print("type of b: ", type(b))
+
+c = "coding"
+print("type of c: ", type(c))
+
+d = True 
+print("type of d: ", type(d))
---
+++ Typecasting/typecasting.py
@@ -0,0 +1,29 @@
+# Assigning different variables
+name = "Penguin"
+age = 15
+is_student = True 
+weight = 38.5
+
+# Printing different variables and their Data Type
+print("Name :", name)
+print("Data type of Name is", type(name))
+
+print("Age :", age)
+print("Data type of Age is", type(age))
+
+print("is_student :", is_student)
+print("Data type of is_student is", type(is_student))
+
+print("Weight :", weight)
+print("Data type of Weight is", type(weight))
+
+# Typecasting to convert the datatype of variables 
+print("in After Type Casting...")
+
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+
+weight = int(weight)
+print(weight)
+print("Data type of weight is", type(weight))
