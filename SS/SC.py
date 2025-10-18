72586dd31d8cecac2979cb17a22513e6956f1cd6
Kairaa2014 <kairaasharma22@gmail.com>
2025-10-18 12:46:39 +0530

    demo

---
+++ SS/SC.py
@@ -0,0 +1,8 @@
+# Take the input 
+text = input("What is the text you want to change, to uppercase? :")
+
+# Run the function
+uppercase = text.upper()
+
+# Print it
+print("The uppercade version is,", uppercase)
